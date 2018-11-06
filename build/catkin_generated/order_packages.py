cc5167479924808d493d82a7c2d6cc86a038dbc1
ironfist16 <khalil.ammour@gmail.com>
2018-11-06 22:02:08 +0400

    Pleasant working of model with four base AX-12A

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
 source_root_dir = "/home/ironfist/Documents/Robot/ROS/dev/aaly_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
+whitelisted_packages = "aaly_urdf".split(';') if "aaly_urdf" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
 underlay_workspaces = "/home/ironfist/Documents/Robot/ROS/dev/aaly_ws/devel;/home/ironfist/Documents/Robot/ROS/dev/simulation_ws/devel;/home/ironfist/Documents/Robot/ROS/dev/baxter_ws/devel;/home/ironfist/Documents/Robot/ROS/dev/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ironfist/Documents/Robot/ROS/dev/aaly_ws/devel;/home/ironfist/Documents/Robot/ROS/dev/simulation_ws/devel;/home/ironfist/Documents/Robot/ROS/dev/baxter_ws/devel;/home/ironfist/Documents/Robot/ROS/dev/catkin_ws/devel;/opt/ros/kinetic" != "" else []
